de5c360ff8f7fdb6c53f5f7bc963f1a82488c131
Ilinca Dumitrascu <ilinca.dumitrascu@gmail.com>
2020-12-22 20:39:51 +0200

    zoom+translate
    Added the scale on the axes + zoom and translate functions to the app

--- draw/draw.cpp
+++ draw/draw.cpp
@@ -3,27 +3,83 @@
 //
 #include "draw.h"
 
-void drawaxes (int lgx, int lgy)
+void nrsir (int n, char s[])
 {
+    int c, nr=0, cn;
+    int k=0;
+
+    cn=n;
+    while(cn!=0)
+    {
+        nr++;
+        cn/=10;
+    }
+    if(n<0)
+    {   s[0]='-';
+        nr++;
+        n=-n;
+    }
+    while(n!=0)
+    {
+        c=n%10;
+        s[nr-k-1]=c+'0';
+        k++;
+        n/=10;
+    }
+    s[nr]='\0';
+}
+
+void drawaxes (int a, int b, int lgx, int lgy, double zoom, int tx, int ty)
+{
+    char sir[1000];
+    int k=0;
 
 ///axele de coordonate
-    line(lgx/2, 0, lgx/2, lgy);
-    line(0, lgy/2, lgx, lgy/2);
+    line(lgx/2*zoom + tx, 0, lgx/2*zoom + tx, lgy);
+    line(0, lgy/2*zoom + ty, lgx, lgy/2*zoom + ty);
 
-///impartirea axelor
-    for(int i=lgx/2; i<=lgx; i+=unitaxpx)
-        line(i, (lgy/2-3), i, lgy/2+3);
+///impartirea si notarea axelor
+    for(int i=lgx/2+unitaxpx; i*zoom + tx<=lgx; i+=unitaxpx)
+    {
+        line(i*zoom + tx, (lgy/2-3)*zoom+ty, i*zoom + tx, (lgy/2+3)*zoom+ty);
+        nrsir(k, sir);
+        outtextxy((i-unitaxpx)*zoom + tx, (lgy/2+3)*zoom+ty, sir);
+        k++;
+    }
+    k=0;
+    for(int i=lgx/2; i*zoom + tx>=0; i-=unitaxpx)
+    {   line(i*zoom + tx, (lgy/2-3)*zoom+ty, i*zoom + tx, (lgy/2+3)*zoom+ty);
+        nrsir(k, sir);
+        outtextxy(i*zoom + tx, (lgy/2+3)*zoom+ty, sir);
+        k--;
+    }
+    k=0;
+    for(int i=lgy/2; i*zoom+ty<=lgy; i+=unitaxpx)
+    {   line((lgx/2-3)*zoom+tx, i*zoom+ty, (lgx/2+3)*zoom+tx, i*zoom+ty);
+        nrsir(k, sir);
+        outtextxy((lgx/2+3)*zoom+tx, i*zoom+ty, sir);
+        k--;
+    }
+    k=0;
+    for(int i=lgy/2; i*zoom+ty>=0; i-=unitaxpx)
+    {   line((lgx/2-3)*zoom+tx, i*zoom+ty, (lgx/2+3)*zoom+tx, i*zoom+ty);
+        nrsir(k, sir);
+        outtextxy((lgx/2 +3)*zoom+tx, i*zoom+ty, sir);
+        k++;
+    }
 
-    for(int i=lgx/2; i>=0; i-=unitaxpx)
-        line(i, (lgy/2-3), i, lgy/2+3);
 
-    for(int i=lgy/2; i<=lgy; i+=unitaxpx)
-        line(lgx/2-3, i, lgx/2+3, i);
+    strcat(sir, "0\0");
 
-    for(int i=lgy/2; i>=0; i-=unitaxpx)
-        line(lgx/2-3, i, lgx/2+3, i);
+    outtextxy((lgx/2+3)*zoom+tx, (lgy/2+3)*zoom+ty, sir);
 
 }
+
+double tr(int coord, double zoom, int translare)
+{
+    return coord*zoom+translare;
+}
+
 ///Functie temporara pentru teste
 double func(double n)
 {
@@ -31,31 +87,34 @@ double func(double n)
 }
 
 ///Deseneaza functie pe intervalul [a, b] intr-o fereastra de dimensiuni lgx, lgy
-void drawf (int a, int b, int unitax, int lgx, int lgy)
+void drawf (int a, int b, double unitax, int lgx, int lgy, double zoom, int tx, int ty)
 {
 
-    /// initwindow(lgx, lgy);
+    cleardevice();
+    drawaxes (a, b, lgx, lgy, zoom, tx, ty);
 
-    drawaxes (lgx, lgy);
 
     ///desenarea graficului
     double x1=lgx/2 + (a/unitax) * unitaxpx;
     double y1=lgy/2 - (func(a)/unitax) * unitaxpx;
     double x2=lgx/2 + ((a+unitf)/unitax) * unitaxpx;
-    double y2=lgy/2 - (func(a+unitf)/unitax) * unitaxpx;
+    double y2=lgy/2 - (func(a + unitf)/unitax) * unitaxpx;
 
     for(double j=a+2*unitf; j<=b; j+=unitf)
     {
         double x3=(j/unitax) * unitaxpx + lgx/2;
-        double y3=lgy/2 - (func(j)/unitax) * unitaxpx;
-        if(sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))<50) ///in cazul in care segmentul este prea mare
-            line(x1, y1, x2, y2);
-        line(x2, y2, x3, y3);
+        double y3=lgy/2-(func(j)/unitax) * unitaxpx;
 
-        if(y2<y1 && y2<y3)
-            circle(x2, y2, 3);
-        if(y2>y1 && y2>y3)
-            circle(x2, y2, 3);
+        if(!((y1 > lgy && y2 < 0) || (y2 > lgy && y1 < 0)))
+        {
+            line(tr(x1, zoom, tx), tr(y1, zoom, ty),
+                 tr(x2, zoom, tx), tr(y2, zoom, ty));
+
+            if(y2<y1 && y2<y3)
+                circle(tr(x2, zoom, tx), tr(y2, zoom, ty), 5*zoom);
+            if(y2>y1 && y2>y3)
+                circle(tr(x2, zoom, tx), tr(y2, zoom, ty), 5*zoom);
+        }
 
         x1=x2;
         y1=y2;
@@ -64,4 +123,43 @@ void drawf (int a, int b, int unitax, int lgx, int lgy)
 
     }
 
+ /**
+  * main example
+  * int a, b;
+    char ch;
+    double unitax, zoom;
+    int tx, ty;
+    tx=0;
+    ty=0;
+    unitax=1;  ///distanta dintre 2 pct pe axa in unitati
+    zoom=1;
+    cout<<"Marginea inferioara a intervalului:";
+    cin>>a;
+    cout<<"Marginea superioara a intervalului:";
+    cin>>b;
+
+    initwindow(800, 1000);
+    do
+    {
+        drawf(a, b, unitax, 800, 1000, zoom, tx, ty);
+        ch=getch();
+        switch(ch)
+        {
+            case '+': zoom=zoom+0.1; break;
+            case '-': zoom=zoom-0.1; break;
+            case 'w':
+            case 72 : ty=ty-5; break;
+            case 's':
+            case 80 : ty=ty+5; break;
+            case 'a':
+            case 75 : tx=tx-5; break;
+            case 'd':
+            case 77 : tx=tx+5; break;
+        }
+    } while(ch!='x');
+
+
+
+    closegraph();
+  */
 }
--- draw/draw.h
+++ draw/draw.h
@@ -4,10 +4,15 @@
 #pragma once
 #include <graphics.h>
 #include <math.h>
+#include <string.h>
 
 const int  unitaxpx=30;
 const double unitf=0.01;
 
-void drawaxes (int lgx, int lgy);
-void drawf (int a, int b, int unitax, int lgx, int lgy); ///Deseneaza functie pe intervalul [a, b] intr-o fereastra de dimensiuni lgx, lgy
+void nrsir (int n, char s[]);
+void drawaxes (int a, int b, int lgx, int lgy, double zoom, int tx, int ty);
+void drawf (int a, int b, double unitax, int lgx, int lgy, double zoom, int tx, int ty); ///Deseneaza functie pe intervalul [a, b] intr-o fereastra de dimensiuni lgx, lgy
 double func(double n); ///Functie temporara pentru teste
+double tr(int coord, double zoom, int translare); /// translate for a point with a zoom
+
+
